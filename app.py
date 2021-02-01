c5a419a4f949734961ce9d098cd0ce85189d067d
UTSA-OIR <Institutional.Research@utsa.edu>
2021-02-01 12:59:11 -0600

    2/1/21 endofDay

--- app.py
+++ app.py
@@ -7,11 +7,30 @@ app = Flask(__name__)                                # define our application
    
 
 
+myDB = mysql.connector.connect(                         # Connecting to SQL Server
+    host="biservdev.utsarr.net",
+    user="fts605",
+    passwd ="",
+    database = "IR_dataRequests"
+)
+
+
+engine = sqlalchemy.create_engine('mysql+pymysql://root:m123rico@localhost/work',)
+
+
 @app.route('/')                                  # url mapping
 def homepage():
  
  
     return render_template("index.html")
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
