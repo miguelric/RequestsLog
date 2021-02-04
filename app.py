dc86f181e0234949690f4e5bb96e5de87adbe68f
UTSA-OIR <Institutional.Research@utsa.edu>
2021-02-04 09:33:17 -0600

    endOfDay 02/04/21
    got rid of pyodbc and now am attempting the pymssql library. Flask outline is set up but cannot get pymssql to download

--- app.py
+++ app.py
@@ -1,32 +1,21 @@
-<<<<<<< HEAD
+
 from flask import Flask, render_template, url_for, request       # Library Importsclear
 import pymssql  
 #import pandas as pd
 #import sqlalchemy
-=======
-from flask import Flask, render_template, url_for, request       # Library Imports
-import pandas as pd
-import sqlalchemy
-
->>>>>>> parent of 4a1cb49... Attempting pyodbc
 
 app = Flask(__name__)                                # define our application
    
 
 
-<<<<<<< HEAD
-conn = pymssql.connect(server='yourserver.database.windows.net', user='yourusername@yourserver', password='yourpassword', database='AdventureWorks')
-=======
-myDB = mysql.connector.connect(                         # Connecting to SQL Server
-    host="biservdev.utsarr.net",
-    user="fts605",
-    passwd ="",
-    database = "IR_dataRequests"
-)
+conn = pymssql.connect(server='biservdev.utsarr.net',
+                            user='fts605', 
+                            password='yourpassword', 
+                            database='IR_dataRequests')
 
 
-engine = sqlalchemy.create_engine('mysql+pymysql://root:m123rico@localhost/work',)
->>>>>>> parent of 4a1cb49... Attempting pyodbc
+
+
 
 
 @app.route('/')                                  # url mapping
@@ -34,11 +23,6 @@ def homepage():
  
  
     return render_template("index.html")
-
-
-
-
-
 
 
 
