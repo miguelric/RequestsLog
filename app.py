67fd19e020f657c602485523194b7bac25129328
UTSA-OIR <Institutional.Research@utsa.edu>
2021-02-05 10:11:48 -0600

    attempted sql alchemy

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
 
-from flask import Flask, render_template, url_for, request       # Library Importsclear
-
-import pypyodbc  
+from flask import Flask, render_template, url_for, request       # Library Imports
+import sqlalchemy
+from sqlalchemy import create_engine
 #import pandas as pd
-#import sqlalchemy
+
+
 
 app = Flask(__name__)                                # define our application
    
+SERVER = '@biservdev.utsarr.net'
+DATABASE = 'IR_dataRequests'
+DRIVER = 'SQL Server Native Client 11.0'
+USERNAME = 'fts605'
+PASSWORD = '123'
+DATABASE_CONNECTION = f'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
 
 
+
+
+engine = create_engine('mssql+pyodbc://fts605:{Password}@biservdev.utsarr.net')
+
+
+"""
 conn = pypyodbc.connect(server='biservdev.utsarr.net',
                             user='fts605', 
                             password='yourpassword', 
                             database='IR_dataRequests')
 
-
+"""
 
 
 
