13524f9f441add2e4b9d52dbf0b0d870fb96aae3
UTSA-OIR <Institutional.Research@utsa.edu>
2021-02-08 12:39:42 -0600

    attempting to connect to DB
    Got help from TechCafe today and got access to needed database. Next step is connecting to python.

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 
 from flask import Flask, render_template, url_for, request       # Library Imports
+import urllib
 import sqlalchemy as db
 from sqlalchemy import create_engine
 import pandas as pd
-
+import pyodbc
 
 
 app = Flask(__name__)                                # define our application
    
+
+
 SERVER = '@biservdev.utsarr.net'
 DATABASE = 'IR_dataRequests'
 DRIVER = 'SQL Server Native Client 11.0'
-USERNAME = 'utsarr/fts605'
-PASSWORD = 'utsarr/rico99miguel1999_'
-DATABASE_CONNECTION = 'mssql://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}'
+USERNAME = 'utsarr\fts605'
+PASSWORD = 'Rico99miguel1999_' 	
 
-engine = db.create_engine(DATABASE_CONNECTION)
-connection = engine.connect()
+#engine = create_engine(f'mssql+pyodbc://{USERNAME}:{PASSWORD}@{SERVER}/{DATABASE}?driver={DRIVER}')
+#connection = engine.connect()
+
+cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+SERVER+';DATABASE='+DATABASE+';UID='+USERNAME+';PWD='+ PASSWORD)
+cursor = cnxn.cursor()
 
 
-engine = create_engine('mssql+pyodbc://fts605:{Password}@biservdev.utsarr.net')
 
 
 
