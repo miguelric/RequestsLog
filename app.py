e7ac67d436f969dd17ca0753d34ebb3f762a283c
UTSA-OIR <Institutional.Research@utsa.edu>
2021-02-22 12:03:48 -0600

    connection successful
    Connection to db was successful. Was also able to connect using sqlalchemy in order to use pandas. Have edited the index.html file to show connection has been successful.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template, url_for, request       # Library Imports
 import urllib
-import sqlalchemy as db
+import sqlalchemy as sa
 from sqlalchemy import create_engine
 import pandas as pd
 import pyodbc
@@ -10,49 +10,38 @@ import pyodbc
 app = Flask(__name__)                                # define our application
    
 
-
-SERVER = '@biservdev.utsarr.net'
-DATABASE = 'IR_dataRequests'
-#DRIVER = 'ODBC Driver 17 for SQL Server'
-#USERNAME = 'utsarr\fts605'
-#PASSWORD = 'Rico99miguel1999_' 	
-
 cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};""Server=BISERVDEV;""Database=IR_dataRequests;""Trusted_Connection=yes;")
 
 cursor = cnxn.cursor()
 
+cursor.execute("SELECT * FROM dbo.requests")
+
+engine = sa.create_engine('mssql+pyodbc://BISERVDEV/IR_dataRequests?driver=SQL Server Native Client 11.0?Trusted_Connection=yes').connect()
+
+
+dic = []
+for row in cursor.fetchall():
+    dic.append(row)
+
+temp = pd.read_sql_table('requests', engine)            
+temp = pd.DataFrame(temp)
+#print(temp) 
 
 
 
+uniqueNames =  temp['assignedTo'].unique()               # Find unique analyst names (currently doesnt work bc of extra analyst names in db)
+uniqueNames = ['Brian Cordeau', 'Ashwin Jayagopal' , 'Fikrewold Bitew', "Jinny Case", 'Lauren Apgar', 'Mahmoud Abunawas' ,'Salma Ferdous']
+numofAnalysts = len(uniqueNames)
 
-
-
-
-@app.route('/')                                  # url mapping
-def homepage():
- 
- 
-    return render_template("index.html")
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-cursor.execute("SELECT * from") 
-row = cursor.fetchone() 
-while row: 
-    print(row[0])
-    row = cursor.fetchone()
-
-
+print(numofAnalysts)
 
 @app.route('/')                                  # url mapping
 def homepage():
+    df = uniqueNames
+    db = dic
  
  
-    return render_template("index.html")
+    return render_template("index.html", df = df, db = db)
 
 
 
